5e54ceeb20107ec918b800af2f93f61a6ca7109b
Ruurd Buijse <ruurdbuijse@MacBook-Pro-van-Ruurd-2.local>
2025-08-07 16:11:14 +0200

    7th commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  server: process.env.DB_HOST
+  server: process.env.DB_HOST,
   database: process.env.DB_NAME,
   options: {
     encrypt: true,
